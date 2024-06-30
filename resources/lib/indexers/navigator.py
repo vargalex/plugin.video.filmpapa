3c43d77033f669d80fd7098579de0938a3ebcec3
Gabor Varga <vargagab@gmail.com>
2024-06-30 20:37:33 +0200

    Forrás változás követése

--- resources/lib/indexers/navigator.py
+++ resources/lib/indexers/navigator.py
@@ -322,23 +322,27 @@ class navigator:
             imdb = re.search(r"([^<]*)(<|$)", imdb).group(1).strip()
         except:
             imdb = None
-        match=re.search(r'<a.*href="(.*?/links/.*?)"', url_content)
+        match=re.search(r'<a.*href="(.*?/lehetosegek/.*?)"', url_content)
         if match:
             linksURL = match.group(1)
             url_content = client.request(linksURL)
-            links = ()
-            try:
-                keremiya_part = client.parseDOM(url_content, 'div', attrs={'class': 'keremiya_part'})[0]
-                spans = client.parseDOM(keremiya_part, 'span')
-                links = client.parseDOM(keremiya_part, 'a', ret = 'href')
-            except:
-                pass
-            if len(links)>0:
-                for idx in range(len(spans)):
-                    link = linksURL if idx == 0 else links[idx-1]
-                    self.addDirectoryItem("%s" % (spans[idx]), 'playmovie&url=%s' % quote_plus(link), thumb, 'DefaultMovies.png', isFolder=False, meta={'title': title, 'plot': plot, 'duration': int(time)*60}, banner=thumb)
-            else:
-                self.addDirectoryItem("%s" % title, 'playmovie&url=%s' % quote_plus(linksURL), thumb, 'DefaultMovies.png', isFolder=False, meta={'title': title, 'plot': plot, 'duration': int(time)*60}, banner=thumb)
+            match=re.search(r'<a.*href="(.*?/links/.*?)"', url_content)
+            if match:
+                linksURL = match.group(1)
+                url_content = client.request(linksURL)
+                links = ()
+                try:
+                    keremiya_part = client.parseDOM(url_content, 'div', attrs={'class': 'keremiya_part'})[0]
+                    spans = client.parseDOM(keremiya_part, 'span')
+                    links = client.parseDOM(keremiya_part, 'a', ret = 'href')
+                except:
+                    pass
+                if len(links)>0:
+                    for idx in range(len(spans)):
+                        link = linksURL if idx == 0 else links[idx-1]
+                        self.addDirectoryItem("%s" % (spans[idx]), 'playmovie&url=%s' % quote_plus(link), thumb, 'DefaultMovies.png', isFolder=False, meta={'title': title, 'plot': plot, 'duration': int(time)*60}, banner=thumb)
+                else:
+                    self.addDirectoryItem("%s" % title, 'playmovie&url=%s' % quote_plus(linksURL), thumb, 'DefaultMovies.png', isFolder=False, meta={'title': title, 'plot': plot, 'duration': int(time)*60}, banner=thumb)
         self.endDirectory('episodes')
 
     def getSearches(self):
